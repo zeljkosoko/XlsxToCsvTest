44a6c89bd00eb459313b9ab501ad05f13baa0e2c
Zeljko Sokolovic <zeljko.sokolovic@zebracon.rs>
2023-01-17 16:19:29 +0100

    download functionality

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using XlsxToCsvTest.Models;
 
@@ -32,6 +34,35 @@ namespace XlsxToCsvTest.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public void Download()
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("accept", "*/*");
+                byte[] filedata = client.DownloadData("http://bakerhughesrigcount.gcs-web.com/static-files/b562fc2a-b229-41eb-8407-54dda5dc7295");
+
+                using (Stream ms = new MemoryStream(filedata))
+                {
+                    string outputFile = Directory.GetCurrentDirectory() + "\\" + "rigCountsFinal.xlsx";
+                    using (var localStream = System.IO.File.Create(outputFile))
+                    {
+                        byte[] buffer = new byte[1024];
+                        int bytesRead;
+
+                        do
+                        {
+                            // Read data (up to 1k) from the stream
+                            bytesRead = ms.Read(buffer, 0, buffer.Length);
+                            // Write the data to the local file
+                            localStream.Write(buffer, 0, bytesRead);
+                            // Increment total bytes processed
+                        } while (bytesRead > 0);
+
+                        localStream.Flush();
+                    }
+                }
+            }
         }
     }
 }
